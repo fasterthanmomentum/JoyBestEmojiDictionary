9ac029a2a338b9cbc3cceed86f2d057f756aac6a
Joy Best <fasterthanmomentum@gmail.com>
2019-09-20 23:11:10 -0400

    set up the labels symbolLabel, nameLabel and descriptionLabel

--- JoyBestEmojiDictionary/EmojiTableViewCell.swift
+++ JoyBestEmojiDictionary/EmojiTableViewCell.swift
@@ -21,4 +21,15 @@ class EmojiTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    @IBOutlet weak var symbolLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func update(with emoji: Emoji) {
+        symbolLabel.text = emoji.symbol
+        nameLabel.text = emoji.name
+        descriptionLabel.text = emoji.description
+    }
 }
