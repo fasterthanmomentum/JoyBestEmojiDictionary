329c17c228595254d81bc1ea9646541ca86dfccf
Joy Best <fasterthanmomentum@gmail.com>
2019-09-20 22:59:26 -0400

    set up the emojitableviewcell

---
+++ JoyBestEmojiDictionary/EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  JoyBestEmojiDictionary
+//
+//  Created by JOY BEST on 9/20/19.
+//  Copyright © 2019 JOY BEST. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
