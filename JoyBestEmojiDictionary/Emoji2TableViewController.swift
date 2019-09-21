5fb4445d565bf1bc4d40b760017d95bd91ce8cec
Joy Best <fasterthanmomentum@gmail.com>
2019-09-21 12:23:53 -0400

    better

--- JoyBestEmojiDictionary/Emoji2TableViewController.swift
+++ JoyBestEmojiDictionary/Emoji2TableViewController.swift
@@ -11,8 +11,19 @@ import UIKit
 class Emoji2TableViewController: UITableViewController {
     
     var emojis: [Emoji] = [
-    Emoji(symbol: "😃", name: "Grinning Face", description: "Grinning Face", usage: "happiness"),
-    Emoji(symbol: "😐", name: "Confused Face", description: "A confused, puzzled face", usage: "unsure what to think; displeasure")]
+        Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face", usage: "happiness"),
+        Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think: displeasure"),
+        Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.", usage: "love of something: attractive"),
+        Emoji(symbol: "👮🏾‍♀️", name: "Police Officer", description: "A police officer wearing a blue cap with gold badge.", usage: "person of authority"),
+        Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle", usage: "Something slow"),
+        Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant", usage: "good memory"),
+        Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spahghetti", usage: "spaghetti"),
+        Emoji(symbol: "🎲", name: "Die", description: "A single die", usage: "taking a risk, chance: game"),
+        Emoji(symbol: "⛺️", name: "Tent", description: "A small tent", usage: "camping"),
+        Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"),
+        Emoji(symbol: "💔", name: "Broken heart", description: "A red, broken heart", usage: "extreme sadness"),
+        Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
+        Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black- and-white checkered flag.", usage: "completion")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
