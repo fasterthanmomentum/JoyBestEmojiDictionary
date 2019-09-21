410ac5a603c8a0f969b9e49a19636d92c7af7f4e
Joy Best <fasterthanmomentum@gmail.com>
2019-09-20 21:58:38 -0400

    Initial Commit

---
+++ JoyBestEmojiDictionary/Emoji.swift
@@ -0,0 +1,21 @@
+//
+//  Emoji.swift
+//  JoyBestEmojiDictionary
+//
+//  Created by JOY BEST on 9/20/19.
+//  Copyright © 2019 JOY BEST. All rights reserved.
+//
+
+import Foundation
+struct Emoji {
+    var symbol: String
+    var name:   String
+    var description:    String
+    var usage:  String
+    init(symbol: String, name: String, description: String, usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
